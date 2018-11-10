7048bb25c2f060387e7fe9b684bb55780c2bfee5
Sricharan <sricharanized@gmail.com>
2018-11-10 22:34:26 +0100

    day1: DS basics

--- ddp.py
+++ ddp.py
@@ -2,6 +2,7 @@ import logging
 import numpy as np
 import time
 import cv2
+import cvui
 import base64
 
 from tf_pose import common
@@ -9,7 +10,7 @@ from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
 
 Estimator = TfPoseEstimator
-
+WINDOW1_NAME = 'Dance Dance Pose'
 posed = 'Images/yoga.jpg'
 
 
@@ -41,6 +42,8 @@ def infer(image, model='mobilenet_thin', resize='368x368', resize_out_ratio=4.0)
     return image
 
 
+frame1 = np.zeros((768, 1024, 3), np.uint8)
+
 inferred = infer(posed)
 original = cv2.imread(posed)
 
@@ -49,6 +52,9 @@ original = cv2.imread(posed)
 #     cv2.imshow('original', original)
 #     if time.time() > timeout_demo:
 #         break
+
+cv2.namedWindow(WINDOW1_NAME)
+cvui.init(WINDOW1_NAME)
 
 time.sleep(5)
 if original.shape[0] != 480 or original.shape[1] != 640:
@@ -61,11 +67,13 @@ inferred=cv2.copyMakeBorder(inferred[:,int(np.nonzero(inferred)[1][0]/2):],0,0,0
 #     break
 timeout = time.time() + 10
 capture = cv2.VideoCapture(0)
-
+counter = [time.time()]
 x = 1
 while True:
+  cvui.context(WINDOW1_NAME)
+
   ret, frame = capture.read()
-  
+  frame1[:] = (49, 52, 49)
   gray = frame
 
   if gray.shape[0] != 480 or gray.shape[1] != 640:
@@ -73,7 +81,22 @@ while True:
 
   dst = cv2.addWeighted(inferred, 0.5, gray, 0.5, 0)
 
-  cv2.imshow('frame', dst)
+  cvui.beginRow(frame1, 10, 20, -1, -1, 30)
+
+  cvui.image(dst)
+  cvui.image(original)
+#   cv2.imshow('frame', dst)
+  cvui.endRow()
+
+  cvui.beginRow(frame1, 10, 400, -1, -1, 30)
+  cvui.counter(frame1, 100, 410, counter, 0.1, '%.1f')
+  counter = [timeout - time.time() for x in counter]
+  cvui.text(frame1, 10, 410, "Tick tick")
+  cvui.endRow()
+
+
+  cvui.update(WINDOW1_NAME)
+  cv2.imshow(WINDOW1_NAME, frame1)
 #   time.sleep(5)
   if cv2.waitKey(1) & 0xFF == ord('q') or time.time() > timeout:
 #   if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -85,11 +108,34 @@ while True:
 
 
 inferred_capture = infer(filename)
+original_inferred=cv2.imread(filename)
+if original_inferred.shape[0] != 4380 or original_inferred.shape[1] != 640:
+    inferred_capture = cv2.resize(inferred_capture, (368, 368))
+    original_inferred = cv2.resize(original_inferred, (368, 368))
 while True:
     final = cv2.addWeighted(inferred, 0.5, inferred_capture, 0.5, 0)
     cv2.imshow('final', final)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+diff_inferred=inferred_capture-original_inferred
+bw_inferred = cv2.cvtColor(diff_inferred, cv2.COLOR_BGR2GRAY)
+bw_inferred[bw_inferred >= 1] = 1
+bw_inferred[bw_inferred < 1] = 0
+
+bw_orig_inferred = cv2.cvtColor(inferred, cv2.COLOR_BGR2GRAY)
+bw_orig_inferred[bw_orig_inferred >= 1] = 1
+bw_orig_inferred[bw_orig_inferred < 1] = 0
+total = bw_orig_inferred == bw_inferred
+#total=np.count_nonzero(bw_inferred)
+#diff_images=bw_orig_inferred-bw_inferred
+#diff_images[diff_images>=1]=1
+#diff_images[diff_images<1]=0
+#reduction=np.count_nonzero(diff_images)
+
+print('')
+print('')
+print(1-np.sum(total)/np.size(total))
 
 def deleteCaptures():
     pass
--- temp.py
+++ temp.py
@@ -1,23 +1,74 @@
+#
+# This demo shows how to use cvui in multiple windows relying on rows and columns.
+#
+# Copyright (c) 2018 Fernando Bevilacqua <dovyski@gmail.com>
+# Licensed under the MIT license.
+#
+
 import numpy as np
 import cv2
-import time
-cap = cv2.VideoCapture(0)
-framerate = cap.get(5)
-x = 1
+import cvui
+import random
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # Our operations on the frame come here
+WINDOW1_NAME = 'Window 1'
+WINDOW2_NAME = 'Windows 2'
 
-    filename = 'captures/capture' + \
-        str(int(x)) + ".png"
-    x = x+1
-    cv2.imwrite(filename, frame)
-    time.sleep(5)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+def main():
+	# We have one mat for each window.
+	frame1 = np.zeros((1024, 768, 3), np.uint8)
+
+	# Create variables used by some components
+	window1_values = []
+	window2_values = []
+
+
+	img = cv2.imread('Images/yoga.jpg', cv2.IMREAD_COLOR)
+	imgRed = cv2.imread('Images/mic.jpg', cv2.IMREAD_COLOR)
+	imgGray = cv2.imread('Images/gamb.jpg', cv2.IMREAD_COLOR)
+	img = cv2.resize(img, (200, 200))
+	imgRed = cv2.resize(imgGray, (200, 200))
+	imgGray = cv2.resize(imgRed, (200, 200))
+		
+	padding = 10
+
+	# Fill the vector with a few random values
+	for i in range(0, 20):
+		window1_values.append(random.uniform(0., 300.0))
+		window2_values.append(random.uniform(0., 300.0))
+
+	# Start two OpenCV windows
+	cv2.namedWindow(WINDOW1_NAME)
+	cv2.namedWindow(WINDOW2_NAME)
+
+	# Init cvui and inform it to use the first window as the default one.
+	# cvui.init() will automatically watch the informed window.
+	cvui.init(WINDOW1_NAME)
+
+	# Tell cvui to keep track of mouse events in window2 as well.
+	cvui.watch(WINDOW2_NAME)
+
+	while (True):
+		# Inform cvui that all subsequent component calls and events are related to window 1.
+		cvui.context(WINDOW1_NAME)
+
+		# Fill the frame with a nice color
+		frame1[:] = (49, 52, 49)
+
+		cvui.beginRow(frame1, 10, 20, -1, -1, 10)
+		cvui.image(img)
+		cvui.button(img, imgGray, imgRed)
+		cvui.endRow()
+
+		# Update all components of window1, e.g. mouse clicks, and show it.
+		cvui.update(WINDOW1_NAME)
+		cv2.imshow(WINDOW1_NAME, frame1)
+
+
+		# Check if ESC key was pressed
+		if cv2.waitKey(20) == 27:
+			break
+
+
+if __name__ == '__main__':
+	main()
